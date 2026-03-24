4b02d496e27d4e0969e49567deae13c49de6a05b
Sheraz Ahmad <sherazahmad7434@gmail.com>
2026-03-24 16:54:25 +0500

    have understanded the routes app route in next

---
+++ next_class2/app/hero/layout.js
@@ -0,0 +1,10 @@
+export default function HeroRootLayout({children}){
+  return(
+    <html>
+      <body>
+        <h1>header fro hearo</h1>
+          {children}
+      </body>
+    </html>
+  )
+}
---
+++ next_class2/app/hero/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const hero = () => {
+  return (
+    <div>
+      hello this is hero
+    </div>
+  )
+}
+
+export default hero
